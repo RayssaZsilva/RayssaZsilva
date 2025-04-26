2775a59d94e92b9c4ef6f9aa3f0d3495564f570b
RayssaZsilva <rayssasteele3@gmail.com>
2025-04-25 22:37:08 -0300

    Update relogio digital main.py

--- relogio digital main.py
+++ relogio digital main.py
@@ -1,16 +1,1 @@
-import tkinter as tk 
-from time import strftime
 
-root = tk.Tk()
-root.title("Relogio Digital")
-
-def relogio():
-    hora = strftime ('%H:%M:%S')
-    label.config(text=hora)
-    label.after(1000, relogio)
-label = tk.Label(root, font=('Helvetica', 48),
-                 background='hex',  foreground='cyan')
-label.pack(anchor='center')
-
-relogio()
-root.mainloop() 
